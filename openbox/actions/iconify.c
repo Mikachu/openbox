da7bb4f48f392f0de617d863913a3707252668a5
Mikael Magnusson <mikachu@comhem.se>
2007-07-24 01:36:14 +0200

    Add "de" option to Iconify action that makes it deiconify windows (useful while alt-tabbing).

--- openbox/actions/iconify.c
+++ openbox/actions/iconify.c
@@ -2,13 +2,23 @@
 #include "openbox/client.h"
 
 static gboolean run_func(ObActionsData *data, gpointer options);
+static gpointer setup_func(xmlNodePtr node);
 
 void action_iconify_startup(void)
 {
     actions_register("Iconify",
-                     NULL, NULL,
+                     setup_func,
+                     NULL,
                      run_func,
                      NULL, NULL);
+}
+
+static gpointer setup_func(xmlNodePtr node)
+{
+    xmlNodePtr n;
+
+    if ((n = obt_parse_find_node(node, "de")))
+        return GINT_TO_POINTER(obt_parse_node_bool(n));
 }
 
 /* Always return FALSE because its not interactive */
@@ -16,7 +26,7 @@ static gboolean run_func(ObActionsData *data, gpointer options)
 {
     if (data->client) {
         actions_client_move(data, TRUE);
-        client_iconify(data->client, TRUE, TRUE, FALSE);
+        client_iconify(data->client, !options, TRUE, FALSE);
         actions_client_move(data, FALSE);
     }
 
