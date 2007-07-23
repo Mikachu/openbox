d0573055cdc26a977c6082a109e5904933254c94
Mikael Magnusson <mikachu@comhem.se>
2007-07-24 01:50:04 +0200

    Disable the evil calling of a kde program on startup

--- openbox/screen.c
+++ openbox/screen.c
@@ -53,7 +53,7 @@
 
 static gboolean screen_validate_layout(ObDesktopLayout *l);
 static gboolean replace_wm(void);
-static void     screen_tell_ksplash(void);
+//static void     screen_tell_ksplash(void);
 static void     screen_fallback_focus(void);
 
 guint           screen_num_desktops;
@@ -299,11 +299,11 @@ gboolean screen_annex(void)
                     NET_SUPPORTED, ATOM, supported, num_support);
     g_free(supported);
 
-    screen_tell_ksplash();
+//    screen_tell_ksplash();
 
     return TRUE;
 }
-
+#if 0
 static void screen_tell_ksplash(void)
 {
     XEvent e;
@@ -337,7 +337,7 @@ static void screen_tell_ksplash(void)
     XSendEvent(obt_display, obt_root(ob_screen),
                False, SubstructureNotifyMask, &e);
 }
-
+#endif
 void screen_startup(gboolean reconfig)
 {
     gchar **names = NULL;
