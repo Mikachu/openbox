7ecfea679ce3483f3e973ffa03c27dbae7cdb09e
Mikael Magnusson <mikachu@comhem.se>
2008-02-27 15:54:26 +0100

    Disable locking for some actions where it didn't make sense.

--- openbox/actions/raiselower.c
+++ openbox/actions/raiselower.c
@@ -11,7 +11,7 @@ void action_raiselower_startup(void)
 /* Always return FALSE because its not interactive */
 static gboolean run_func(ObActionsData *data, gpointer options)
 {
-    if (!actions_client_locked(data)) {
+    if (data->client) {
         actions_client_move(data, TRUE);
         stacking_restack_request(data->client, NULL, Opposite);
         actions_client_move(data, FALSE);
