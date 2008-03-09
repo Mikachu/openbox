b5c9cfd1ff151fda6f8f5c2ed9a8214e22550837
Mikael Magnusson <mikachu@comhem.se>
2008-03-09 20:38:43 +0100

    Add FocusFallback action.

---
+++ openbox/actions/focusfallback.c
@@ -0,0 +1,17 @@
+#include "openbox/actions.h"
+#include "openbox/focus.h"
+
+static gboolean run_func(ObActionsData *data, gpointer options);
+
+void action_focusfallback_startup(void)
+{
+    actions_register("FocusFallback", NULL, NULL, run_func, NULL, NULL);
+}
+
+/* Always return FALSE because its not interactive */
+static gboolean run_func(ObActionsData *data, gpointer options)
+{
+    if (data->client && data->client == focus_client)
+        focus_fallback(FALSE, FALSE, TRUE, FALSE);
+    return FALSE;
+}
