ec54e1fa6a0855353e2f980afc124d83413996dc
Mikael Magnusson <mikachu@gmail.com>
2008-10-04 12:49:56 +0200

    Don't allow deiconifying locked windows via the focus action.

--- openbox/actions/focus.c
+++ openbox/actions/focus.c
@@ -43,9 +43,11 @@ static gboolean run_func(ObActionsData *data, gpointer options)
             (data->context != OB_FRAME_CONTEXT_CLIENT &&
              data->context != OB_FRAME_CONTEXT_FRAME))
         {
-            actions_client_move(data, TRUE);
-            client_activate(data->client, o->here, FALSE, FALSE, TRUE);
-            actions_client_move(data, FALSE);
+            if (!(data->client->iconic && actions_client_locked(data))) {
+                actions_client_move(data, TRUE);
+                client_activate(data->client, o->here, FALSE, FALSE, TRUE);
+                actions_client_move(data, FALSE);
+            }
         }
     } else if (data->context == OB_FRAME_CONTEXT_DESKTOP) {
         /* focus action on the root window. make keybindings work for this
